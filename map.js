70916b846477149c893a32cbe1f54a1cf4bc0015
DavidAMills <david@davidmills.io>
2019-12-17 08:55:33 -0600

    Mapbox Terrain

--- map.js
+++ map.js
@@ -5,7 +5,7 @@ var mymap = L.map('mapid').setView(hong_kong_center, 12);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   Zoom: 20,
   maxZoom: 20,
-  id: 'mapbox.streets'
+  id: 'mapbox.mapbox-terrain-v2'
 }).addTo(mymap);
 
 /*
