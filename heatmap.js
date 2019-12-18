2c0c496f12d1558186851498e824035fcb064108
DavidAMills <david@davidmills.io>
2019-12-18 08:18:13 -0600

    Reorganized for load order purposes.

--- heatmap.js
+++ heatmap.js
@@ -5,3 +5,17 @@
 
 // Runs the locationParser function that gets the wind data.
   locationParser();
+
+  function addWindStations() {
+    // Add the Weather stations with wind to the map.
+    for(i = 0; i < AWS_Station.length; i++)
+    {
+      for(z = 0; z < wind_name.length; z++)
+      {
+        if(AWS_Station[i].StationName.name_E == wind_name[z])
+        {
+          L.marker([parseFloat(AWS_Station[i].latitude),parseFloat(AWS_Station[i].longitude)]).addTo(mymap).bindPopup("<b>" + AWS_Station[i].StationName.name_UC + "</b><br>Wind Speed: " + wind_speed[z] + "<br> Wind Direction: " + wind_direction[z]);
+        }
+      }
+    }
+  }
