0c024978bfdaafa25289c89f36b7ae8451f1ee48
DavidAMills <david@davidmills.io>
2019-12-18 07:58:00 -0600

    Adding wind stations to map.

--- map.js
+++ map.js
@@ -8,10 +8,15 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
   id: 'mapbox.mapbox-terrain-v2'
 }).addTo(mymap);
 
-/*
+// Add the Weather stations with wind to the map.
 for(i = 0; i < AWS_Station.length; i++)
 {
-  L.marker([parseFloat(AWS_Station[i].latitude),parseFloat(AWS_Station[i].longitude)]).addTo(mymap)
-  .bindPopup("<b>" + AWS_Station[i].StationName.name_UC + "</b><br />Weather Data Here.");
+  for(z = 0; z < wind_name.length; z++)
+  {
+    if(AWS_Station[i].StationName.name_E == wind_name[z])
+    {
+      L.marker([parseFloat(AWS_Station[i].latitude),parseFloat(AWS_Station[i].longitude)]).addTo(mymap)
+      .bindPopup("<b>" + AWS_Station[i].StationName.name_UC + "</b>Wind Speed: " + wind_speed[z] + "<br> Wind Direction: " + wind_direction[z]);
+    }
+  }
 }
-*/
