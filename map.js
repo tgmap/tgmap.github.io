dd258f173ca6d3e04403549c527a53c919aaebbe
DavidAMills <david@davidmills.io>
2019-12-20 22:11:56 -0600

    Digital Topographic Leaflet API

--- map.js
+++ map.js
@@ -2,9 +2,9 @@ var hong_kong_center = [22.3693,114.1694];
 
 var mymap = L.map('mapid').setView(hong_kong_center, 12);
 
-var wms_server = "https://api.hkmapservice.gov.hk/ags/map/basemap/WGS84r?";
-var wms_options = { layers: "0", format: "image/png"}
+var apikey = '584b2fa686f14ba283874318b3b8d6b0'
 
-var topographic = L.tileLayer.wms(wms_server, wms_options);
-
-topographic.addTo(mymap);
+L.tileLayer('https://api.hkmapservice.gov.hk/osm/xyz/label-tc/WGS84/tile/{z}/{x}/{y}.png?key=' + apikey, {
+  maxZoom: 20,
+  id: 'APIKEY'
+}).addTo(mymap);
