2082481c1ad22a0435f3fee56b73948bade393b5
DavidAMills <david@davidmills.io>
2019-12-20 22:21:41 -0600

    Removed un-necessary call to locationParser, updated to Stamen Layer.

--- heatmap.js
+++ heatmap.js
@@ -2,6 +2,3 @@
   var heat = L.heatLayer(
     heat_map_variables
   , {radius:25}).addTo(mymap);
-
-// Runs the locationParser function that gets the wind data.
-  locationParser();
--- map.js
+++ map.js
@@ -2,9 +2,11 @@ var hong_kong_center = [22.3693,114.1694];
 
 var mymap = L.map('mapid').setView(hong_kong_center, 12);
 
-var apikey = '584b2fa686f14ba283874318b3b8d6b0'
+var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+   minZoom: 0,
+   maxZoom: 20,
+   ext: 'png'
+});
 
-L.tileLayer('https://api.hkmapservice.gov.hk/osm/xyz/label-tc/WGS84/tile/{z}/{x}/{y}.png?key=' + apikey, {
-  maxZoom: 20,
-  id: 'APIKEY'
-}).addTo(mymap);
+mymap.addLayer(Stamen_Toner);
