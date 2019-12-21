3dbdd1d5519427ef1fc97c0391db5cdc28b7ce29
DavidAMills <david@davidmills.io>
2019-12-20 22:03:35 -0600

    Changed to a topographic map.

--- map.js
+++ map.js
@@ -2,8 +2,9 @@ var hong_kong_center = [22.3693,114.1694];
 
 var mymap = L.map('mapid').setView(hong_kong_center, 12);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-  Zoom: 20,
-  maxZoom: 20,
-  id: 'mapbox.mapbox-terrain-v2'
-}).addTo(mymap);
+var wms_server = "http://basemap.nationalmap.gov/arcgis/services/USGSTopo/MapServer/WMSServer?";
+var wms_options = { layers: "0", format: "image/png", attribution: "USGS"}
+
+var topographic = L.tileLayer.wms(wms_server, wms_options);
+
+topographic.addTo(mymap);
