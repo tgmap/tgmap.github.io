82a4dc3edb9f7ee7716238a4246ffc380aa83605
DavidAMills <david@davidmills.io>
2019-12-20 22:07:00 -0600

    Using a Hong Kong server for topography.

--- map.js
+++ map.js
@@ -2,8 +2,8 @@ var hong_kong_center = [22.3693,114.1694];
 
 var mymap = L.map('mapid').setView(hong_kong_center, 12);
 
-var wms_server = "http://basemap.nationalmap.gov/arcgis/services/USGSTopo/MapServer/WMSServer?";
-var wms_options = { layers: "0", format: "image/png", attribution: "USGS"}
+var wms_server = "https://api.hkmapservice.gov.hk/ags/map/basemap/WGS84r?";
+var wms_options = { layers: "0", format: "image/png"}
 
 var topographic = L.tileLayer.wms(wms_server, wms_options);
 
