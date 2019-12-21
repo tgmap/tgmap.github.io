573f711f762c97dad4b41ce8b62f98f01b7d3dec
DavidAMills <david@davidmills.io>
2019-12-20 22:24:40 -0600

    Changed to terrain from toner.

--- map.js
+++ map.js
@@ -2,9 +2,8 @@ var hong_kong_center = [22.3693,114.1694];
 
 var mymap = L.map('mapid').setView(hong_kong_center, 12);
 
-var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-   minZoom: 0,
    maxZoom: 20,
    ext: 'png'
 });
