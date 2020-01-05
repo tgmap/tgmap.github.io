b4b0926b8cbb59e0bad7451ce180affdc9db942b
DavidAMills <david@davidmills.io>
2020-01-04 20:42:51 -0600

    Changing location of "rotationAngle" property.

--- variables.js
+++ variables.js
@@ -199,7 +199,8 @@ var Northwest = L.divIcon({
 
 var TestWind = L.divIcon({
   className: 'East',
-  iconSize: [50, 50]
+  iconSize: [50, 50],
+  rotationAngle: 90
 })
 
 // Rounds to the nearest number divisible by 5.
@@ -291,7 +292,7 @@ async function weatherAsync() {
         {
           if(AWS_Station[i].StationName.name_E == wind_name[z])
           {
-            L.marker([parseFloat(AWS_Station[i].latitude),parseFloat(AWS_Station[i].longitude)], {icon: TestWind}, {rotationAngle: 45}).addTo(mymap).bindPopup("<b>" + AWS_Station[i].StationName.name_UC + "</b><br>Wind Speed: " + wind_speed[z] + "<br> Wind Direction: " + wind_direction[z]);
+            L.marker([parseFloat(AWS_Station[i].latitude),parseFloat(AWS_Station[i].longitude)], {icon: TestWind}).addTo(mymap).bindPopup("<b>" + AWS_Station[i].StationName.name_UC + "</b><br>Wind Speed: " + wind_speed[z] + "<br> Wind Direction: " + wind_direction[z]);
           }
         }
       }
